be50394ad84966f32d01a83c9e7db4f6a6405516
omid <omid.mba@gmail.com>
2022-03-24 18:49:29 +0430

    first commit

---
+++ src/main/java/com/example/oauth2/controller/CoreRestController.java
@@ -0,0 +1,10 @@
+package com.example.oauth2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/core")
+public class CoreRestController {
+
+}
